f6d62606e036f45674a30ccc65b720aa788b808b
AlisevchAndrew <dopysk@yandex.ru>
2024-04-29 20:58:03 +0300

    task and decision of the 'set'

--- self_taught.py
+++ self_taught.py
@@ -1,49 +1,32 @@
 
-# ==================================================
-# import of modul
-# import datetime
-# print(datetime.MAXYEAR)
+# Task of 'set'
+# 1. Create set from different element of 'int'
+# 2. Add to it another one element
+# 3. Create other set with some of elements, notes:
+# some elements of other set, must includ the similar
+# elements of the first set
+# 4. Find the common elements in the both of elements
+# (use the method 'intersection' (&))
+# and put them into another new set
+# 5. make the conversion common set into the list ([])
+# and entering the list into the terminal
+# 6. Make another operations with resault set (by your wishes)
 
-# or through function
-import datetime
+# decision (i'm)
+my_set = {12, 20}
+my_set.add(45)
+print(my_set)
 
+other_set = {20, 50, 7, 12}
 
-def new_func():
-    return datetime.MAXYEAR
-
-
-print(new_func())
-
-# =================================================
-
-# dict
-my_dict = {}
-
-key_1 = input("Please enter key 1: ")
-value_1 = input("Please enter value 1: ")
-
-my_dict[key_1] = value_1
-# если будет запись в формате 'key_1' - то результат будет key_1
-# а мы ключу присваиваем значение переменной key_1, то есть названия, которое введее после
-# "Please enter key 1: "
-
-key_2 = input("Please enter key 2: ")
-value_2 = input("Please enter value 2: ")
-
-my_dict[key_2] = value_2
-
-key_3 = input("Please enter key 3: ")
-value_3 = input("Please enter value 3: ")
-
-my_dict[key_3] = value_3
-
-# adding two keys
-my_dict['key_4'] = 'value_4'
-my_dict['key_5'] = 'value_5'
-
-# deleting two keys
-del my_dict[key_3]
-del my_dict['key_4']
-
-print(my_dict)
-# ----------------
+common_set = my_set & other_set
+print(common_set)
+common_set = my_set.intersection(other_set)
+print(common_set)
+common_set = list(common_set)
+print(common_set)
+print(type(common_set))
+common_set = set(common_set)
+print(type(common_set))
+common_set.remove(12)
+print(common_set)
